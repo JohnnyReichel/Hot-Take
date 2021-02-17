13eb432a8b0219cae817c91ab03437e3c742b80b
John Reichel <jreichel447@gmail.com>
2021-02-16 19:45:25 -0600

    Initial Commit

---
+++ Hot Take/Hot_TakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_TakeApp.swift
+//  Hot Take
+//
+//  Created by John Reichel on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Hot_TakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
